99708fd44006132b74084ff36795b87ba50de352
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2025-02-01 22:46:59 +0545

    dsafsdaf

--- components/Ads/BannerAds.js
+++ components/Ads/BannerAds.js
@@ -6,42 +6,42 @@ function BannerAds() {
     var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
     var uniqid = randLetter + Date.now();
     return (
-        <div className="w-screen overflow-hidden">
+        <div className="">
 
-            {/* <Script
+            <Script
                 type="text/javascript"
                 src="//cdn.tsyndicate.com/sdk/v1/bi.js"
-                data-ts-spot="da8ab6fd16ed4d67a5b88a31e2d79a1a"
+                data-ts-spot="9d2353bb144e44528acb154218a20ded"
                 data-ts-width="300"
                 data-ts-height="250"
                 data-ts-extid="{extid}"
                 async
                 defer
-            /> */}
+            />
 
-            
+
             {/* Exoclick Multiformat  */}
 
-
-
             {/* <Script
-                strategy="afterInteractive"
-                src="https://a.magsrv.com/ad-provider.js"
                 async
                 type="application/javascript"
-            /> */}
+                src="https://a.magsrv.com/ad-provider.js"
+                strategy="afterInteractive" // Ensures the script runs after the page is interactive
+            />
+            <Script
+                id="ad-provider-init"
+                strategy="afterInteractive" // Ensures this script runs after the external script
+            >
+                {`
+        (window.AdProvider = window.AdProvider || []).push({ "serve": {} });
+      `}
+            </Script>
+            <ins className="eas6a97888e38" data-zoneid="5390772"></ins> */}
 
-            {/* Inline script to initialize the AdProvider */}
-            {/* <Script
-            id={uniqid}
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `(AdProvider = window.AdProvider || []).push({ "serve": {} });`,
-                }}
-            /> */}
 
-            {/* The ad element */}
-            {/* <ins className="eas6a97888e38" data-zoneid="5517814"></ins> */}
+
+
+  
 
 
         </div>
--- components/Ads/Popunder.js
+++ components/Ads/Popunder.js
@@ -7,11 +7,6 @@ function PopunderAds() {
         currentHost = window.location.host;
     }
 
-    var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-    var uniqid = randLetter + Date.now();
-
-
-
     // Check if the app is running on localhost
     const isLocalhost = currentHost.includes('localhost')
 
@@ -19,31 +14,18 @@ function PopunderAds() {
     return (
         <div className="flex items-center justify-center">
             {!isLocalhost && (
-                // <Script
-                //     type="text/javascript"
-                //     src="//cdn.tsyndicate.com/sdk/v1/p.js"
-                //     data-ts-spot="f2a620cec4b04527ad64dae7221354ce"
-                //     data-ts-extid="{extid}"
-                //     data-ts-session-duration="300"
-                //     data-ts-count="5"
-                //     data-ts-mode="selective"
-                //     data-ts-ignore-filter="block_popunder"
-                //     async
-                //     defer
-                // />
-
-//Exoclick ads
-                <>
-                    {/* <Script
-                        src="/popunderAds.js"
-                        strategy="lazyOnload"
-                        onLoad={() => {
-                            console.log("Popunder Ad configuration script loaded successfully.");
-                        }}
-                    /> */}
-
-                </>
-
+                <Script
+                    type="text/javascript"
+                    src="//cdn.tsyndicate.com/sdk/v1/p.js"
+                    data-ts-spot="637f41ccc95a46769276024b3fe07174"
+                    data-ts-extid="{extid}"
+                    data-ts-session-duration="300"
+                    data-ts-count="5"
+                    data-ts-mode="selective"
+                    data-ts-ignore-filter="block_popunder"
+                    async
+                    defer
+                />
             )}
         </div>
     );
