5862e1f60d35e6f042b144751fb96622ef5ac62e
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2025-02-20 11:22:03 +0545

    fdgfd

--- components/Banner_for_chutlund2.js
+++ components/Banner_for_chutlund2.js
@@ -44,9 +44,9 @@ export default function Banner_for_chutlund2() {
         ) go to our alternate website{' '}
 
 
-        <a href="https://chutlunds1.com" className="underline text-yellow-500 font-inter">Chutlunds1.com </a>
+        <a href="https://chutlunds4.com" className="underline text-yellow-500 font-inter">chutlunds4.com </a>
         ,
-        <a href="https://Fuckvideo.live" className="ml-1 underline text-yellow-500 font-inter">Fuckvideo.live</a>
+        <a href="https://cumcraze1.com" className="ml-1 underline text-yellow-500 font-inter">cumcraze1.com</a>
 
       </span>
 
